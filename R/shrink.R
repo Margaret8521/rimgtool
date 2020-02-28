2969c52ed0fb4acdd80eeee143a3e4cd45f49382
rita-ni <55466310+rita-ni@users.noreply.github.com>
2020-02-28 13:35:28 -0800

    Update shrink.R

--- R/shrink.R
+++ R/shrink.R
@@ -1,14 +1,16 @@
-#' Return a shrinked image
+#' Given a image and specified height and width, this function shrink the input
+#' image to the specified height and width and return the new image as array.
 #'
-#' @param img array
-#' @param n integer
+#' @param img input image as a array
+#' @param width integer desired width for new image
+#' @param height integer desired height for new image
 #'
-#' @return array
-#' @export
+#' @return array for shrinked image
 #'
 #' @examples
 #' old_img <- array(1:24, dim = c(3, 4, 2))
 #' (shrinked_img <- shrink(old_img))
+
 shrink <- function(img, n) {
   img
 }
