e22c43850cff704b6b273796f04e8d5799cb760f
margaret8521 <margaretzhao@yahoo.com>
2020-03-05 22:34:15 -0800

    add tests for the compress function

--- tests/testthat/test-compress.R
+++ tests/testthat/test-compress.R
@@ -1,5 +1,21 @@
-test_that("compress works", {
-  old_img <- array(1:24, dim = c(3, 4, 2))
+img <- array(1:24, dim = c(4, 4, 3))
+img_wrong <- array(1:24, dim = c(4, 4))
 
-  expect_identical(sharpen(old_img), old_img)
+test_that("test the output image of the compress function", {
+  expect_equal(dim(compress(img, 2))[1], dim(img)[1])
+  expect_equal(length(dim(compress(img, 2))), 3)
+  expect_equal(dim(compress(img, 2))[2], dim(img)[2])
 })
+
+test_that("test the error handling when invalid inputs are entered", {
+  expect_error(compress(img, 5.1))
+  expect_error(compress(img_wrong, 2))
+  expect_error(compress(img, -3))
+})
+
+
+
+
+
+
+
