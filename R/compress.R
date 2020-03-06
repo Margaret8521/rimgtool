48e68224727ef7bf311a433e71ede36179d24c78
margaret8521 <margaretzhao@yahoo.com>
2020-03-05 22:33:38 -0800

    add the complete compress function

--- R/compress.R
+++ R/compress.R
@@ -1,4 +1,8 @@
 #' Return a compressed image
+#' Quantizes an image into 2^b clusters and
+#' return a version of the image (the same size as the original) 
+#' where each pixel's original colour is replaced with the nearest 
+#' prototype colour.
 #'
 #' @param img array, the image to be processed
 #' @param n integer, the desired number of bits
@@ -9,6 +13,39 @@
 #' @examples
 #' old_img <- array(1:24, dim = c(3, 4, 2))
 #' (compressed_img <- compress(old_img))
-compress <- function(img, n) {
-  img
+#' 
+
+
+# compress function
+compress <- function(img, b) {
+  
+  # check if input is valid
+  if ((is.array(img) == FALSE) | (length(dim(img)) != 3)) {
+    stop("TypeError: The dimension of the input image must be 3D array")
+  }
+  
+  if (is.integer(b) == FALSE) {
+    stop("TypeError: b should be an integer!")
+  }
+  
+  if (b <= 0) {
+    stop("ValueError: b should be positive")
+  }
+  
+  # reshape the image
+  image_array <- array_reshape(img, dim = c(dim(v)[1]*dim(v)[2], dim(v)[3]))
+  # use kmeans for compression
+  model <- kmeans(image_array, 4)
+  # reshape labels
+  quantized_img <- array_reshape(model$cluster, dim = c(dim(v)[1], dim(v)[2]))
+  # find the cluster centers
+  colours <- model$centers
+  # dequantized where the original color is replaced 
+  # with the nearest prototype colour
+  image <- colours[quantized_img, ]
+  image <- array_reshape(image, dim = c(dim(v)[1], dim(v)[2], dim(v)[3]))
+  
+  return(image)
 }
+
+
