def638868cf1f07ea7769cc38587dfcb0d0e6f6d
Frank Lu <franklu@live.ca>
2020-03-06 21:04:37 -0800

    Add two tests for 100% coverage

--- tests/testthat/test-sharpen.R
+++ tests/testthat/test-sharpen.R
@@ -1,6 +1,8 @@
 old_img <- array(rep(c(0.5), n = (100*100*3)), dim = c(100, 100, 3))
 new_img <- sharpen(old_img)
 invalid_img <- array(rep(c(1.1), n = (30*30*3)), dim = c(30, 30, 3))
+fake_img <- list(rep(c(0.5), 100), rep(c(0.3), 100), rep(c(0.9), 3))
+out_of_bound <- array(rep(c(1.5), n = (100*100*3)), dim = c(100, 100, 3))
 
 test_that("sharpen works", {
   expect_identical(dim(new_img)[1], dim(old_img)[1])
@@ -15,6 +17,14 @@ test_that("new image dimension is correct", {
   expect_equal(dim(new_img)[1:2], dim(new_img)[1:2])
 })
 
-test_that("yield error when input type is invalid", {
+test_that("yield error when input type is too small", {
   expect_error(sharpen(invalid_img))
+})
+
+test_that("yield error when input type is not array", {
+  expect_error(sharpen(fake_img))
+})
+
+test_that("yield error when brightness is out of bound", {
+  expect_error(sharpen(out_of_bound))
 })
--- tests/testthat/test-shrink.R
+++
@@ -1,5 +0,0 @@
-test_that("shrink works", {
-  old_img <- array(1:24, dim = c(3, 4, 2))
-
-  expect_identical(sharpen(old_img), old_img)
-})
